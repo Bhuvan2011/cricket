fe07f7625466892ff90f3331a417cadf103e2733
bhuvan2011 <puri.bhuvan@yahoo.co.in>
2013-01-04 13:54:55 -0500

    initial commit

---
+++ app/models/player.rb
@@ -0,0 +1,5 @@
+class Player < ActiveRecord::Base
+  attr_accessible :name
+
+  validates_presence_of :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cricket::Application.config.secret_token = '5a97bfa4bb60946c5fe65a4fd34984a734b93f756cf6c98d06586198c41943d2fd7a0dda828f1ad684143635ec288c5278e5a67eab716e9cbb793e9af9c5f2d2'
